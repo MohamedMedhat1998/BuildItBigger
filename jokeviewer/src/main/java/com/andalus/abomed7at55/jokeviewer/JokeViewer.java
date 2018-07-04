98e92eaf41b01b4c9684f182484cd87acb5e4cae
Abo_Med7at <abomed7at55@gmail.com>
2018-07-04 17:45:05 +0200

    Review notes applied

--- jokeviewer/src/main/java/com/andalus/abomed7at55/jokeviewer/JokeViewer.java
+++ jokeviewer/src/main/java/com/andalus/abomed7at55/jokeviewer/JokeViewer.java
@@ -3,6 +3,7 @@ package com.andalus.abomed7at55.jokeviewer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class JokeViewer extends AppCompatActivity {
 
@@ -15,9 +16,13 @@ public class JokeViewer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_viewer);
         tvJokeView = findViewById(R.id.tv_joke);
+        try {
+            String joke = getIntent().getExtras().getString(JOKE_KEY);
+            tvJokeView.setText(joke);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), R.string.error_message,Toast.LENGTH_SHORT).show();
+        }
 
-        String joke = getIntent().getExtras().getString(JOKE_KEY);
-
-        tvJokeView.setText(joke);
     }
 }
